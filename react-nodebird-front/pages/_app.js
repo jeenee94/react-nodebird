f022c21f6932e5281351918a65055febfd13dd91
jeenee <woojin7124@naver.com>
2020-07-30 15:34:33 +0900

    Add: redux saga

--- react-nodebird-front/pages/_app.js
+++ react-nodebird-front/pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Proptypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
+import withReduxSaga from 'next-redux-saga';
 
 import wrapper from '../store/configureStore';
 
@@ -20,4 +21,4 @@ App.prop_types = {
   Component: Proptypes.elementType.isRequired,
 };
 
-export default wrapper.withRedux(App);
+export default wrapper.withRedux(withReduxSaga(App));
