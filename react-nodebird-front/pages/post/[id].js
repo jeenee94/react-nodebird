2498df604a76eefc6e2f5e7b59ccc4ac03c7e29d
jeenee <woojin7124@naver.com>
2020-08-15 14:53:45 +0900

    Update: og tag

--- react-nodebird-front/pages/post/[id].js
+++ react-nodebird-front/pages/post/[id].js
@@ -34,10 +34,10 @@ const Post = () => {
           content={
             singlePost.Images[0]
               ? singlePost.Images[0].src
-              : 'https://nodebird.com/favicon.ico'
+              : 'http://nodebird.ml/favicon.ico'
           }
         />
-        <meta property="og:url" content={`https://nodebird.com/post/${id}`} />
+        <meta property="og:url" content={`http://nodebird.ml/post/${id}`} />
       </Head>
       <PostCard post={singlePost} />
     </AppLayout>
--- react-nodebird-front/pages/user/[id].js
+++ react-nodebird-front/pages/user/[id].js
@@ -57,8 +57,8 @@ const User = () => {
           property="og:description"
           content={`${userInfo.nickname}님의 게시글`}
         />
-        <meta property="og:image" content="https://nodebird.com/favicon.ico" />
-        <meta property="og:url" content={`https://nodebird.com/user/${id}`} />
+        <meta property="og:image" content="http://nodebird.ml/favicon.ico" />
+        <meta property="og:url" content={`http://nodebird.ml/user/${id}`} />
       </Head>
       {userInfo ? (
         <Card
