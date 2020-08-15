46e23c03cbfac2ccc52406604540c66f70dc35aa
jeenee <woojin7124@naver.com>
2020-08-15 14:20:08 +0900

    Fix: typo

--- lambda/index.js
+++ lambda/index.js
@@ -5,7 +5,7 @@ const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
-  const Key = event.Records[0].object.key;
+  const Key = event.Records[0].s3.object.key;
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1];
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
